f80c2dc16dbfd142f29a0d255dfaeda6e1dccd01
周成队长 <CPZHOUCHENG@192-168-1-104.tpgi.com.au>
2017-09-24 14:46:32 +1000

    Initial Commit

---
+++ Assign2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assign2
+//
+//  Created by 周成队长 on 24/9/17.
+//  Copyright © 2017 Jeff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
